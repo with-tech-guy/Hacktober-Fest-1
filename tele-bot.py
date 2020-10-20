52943cea7c910f3e9bcbe55af4263fa7bfe5b10c
Niteshbots <65546540+Niteshbots@users.noreply.github.com>
2020-10-20 20:37:54 +0530

    Introducing to Telegram bot with python

---
+++ tele-bot.py
@@ -0,0 +1,44 @@
+#(c)NKB(Niteshbots)[2020]
+#Copy With Credits
+#
+
+from telegram.ext import Filters,Updater,MessageHandler,CommandHandler
+from commands.command_filter import *
+from functions.bot_func import *
+from caption.set_caption import *
+import os
+
+TOKEN= "YOUR_BOT_TOKEN_HERE"
+
+updater=Updater(TOKEN,use_context=True)
+dp=updater.dispatcher
+
+
+#/start
+dp.add_handler(CommandHandler('start',start_text))
+#/help
+dp.add_handler(CommandHandler('help',help_text))
+
+#Files
+dp.add_handler(MessageHandler(Filters.document,frwrd_file))
+
+#Media
+dp.add_handler(MessageHandler(Filters.video,frwrd_media))
+
+#Photos
+dp.add_handler(MessageHandler(Filters.photo,frwrd_photo))
+
+#Text & Caption
+dp.add_handler(MessageHandler(Filters.text,set_caption))
+
+#Stickers
+dp.add_handler(MessageHandler(Filters.sticker,frwrd_sticker))
+
+#Voice
+dp.add_handler(MessageHandler(Filters.voice,frwrd_voice))
+
+#Audio
+dp.add_handler(MessageHandler(Filters.audio,frwrd_audio))
+
+updater.start_polling()
+updater.idle()
